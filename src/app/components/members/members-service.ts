43d401afd72d17804374256f76c0c56fb55d0d63
Felipe Sangiorge <felipe.sangiorge@marvil56.com>
2020-04-07 10:03:42 +0100

    heroki

--- src/app/components/members/members-service.ts
+++ src/app/components/members/members-service.ts
@@ -1,22 +1,23 @@
+import { User } from './../../model/user-model';
+
 import {Injectable} from '@angular/core';
 import { HttpClient} from '@angular/common/http';
 
 import { GATE_API } from '../../app-api';
 import {ErrorHandler} from '../../app-error-handler';
-import { User } from 'src/app/model/user-model';
 
 import { map } from 'rxjs/operators';
 import { catchError } from 'rxjs/operators';
 import { Observable } from 'rxjs/internal/Observable';
-
 @Injectable ()
 export class MembersService {
 
   constructor(private http: HttpClient){}
 
 
-users(): Observable<User> {
+users(): Observable<User[]> {
   return this.http.get(`${GATE_API}/users`)
+  .pipe(map((response: User[]) => response))
   .pipe(catchError(ErrorHandler.handleError));
 }
 
